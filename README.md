b27ae0cb334183b1266b398f710c7dfd472cc85a
Justin Zablocki <zablockijj@gmail.com>
2019-02-10 12:46:15 -0500

    Create stubbed config file for publish

---
+++ README.md
@@ -0,0 +1,54 @@
+# Laravel Jetpack
+
+A package to make rapid prototyping even easier.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg?style=flat-square)](https://opensource.org/licenses/MIT)
+
+* [Background](#background)
+* [Installation](#installation)
+* [Copyright and License](#copyright-and-license)
+
+
+## Background
+
+This is background
+
+## Installation
+
+**Requirements**: This package requires the following version of Laravel:
+> 
+> | Laravel Version |
+> |:---------------:|
+> |       5.7       |
+
+1. Add the following to the `repositories` section of you `composer.json` file:
+
+    ```sh
+    "repositories": [
+        {
+            "type": "vcs",
+            "no-api": true,
+            "url": "git@github.com:zablockibros/laravel-jetpack.git"
+        }
+    ],
+    ```
+    
+2. Install the package via Composer:
+
+    ```sh
+    $ composer require zablockibros/laravel-jetpack:dev-master
+    ```
+
+    The package will automatically register its service provider.
+
+3. Optionally, publish the configuration file if you want to change any defaults:
+
+    ```sh
+    php artisan vendor:publish --provider="ZablockiBros\Jetpack\JetpackServiceProvider"
+    ```
+
+## Copyright and License
+
+[MIT License](LICENSE.md).
+
+Copyright (c) 2019 Justin Zablocki
--- src/JetpackServiceProvider.php
+++ src/JetpackServiceProvider.php
@@ -11,13 +11,8 @@ class JetpackServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (! class_exists('CreateJetpackTables')) {
-            $timestamp = date('Y_m_d_His', time());
-
-            $this->publishes([
-                __DIR__.'/../database/migrations/create_jetpack_tables.php.stub' => database_path('migrations/'.$timestamp.'_create_jetpack_tables.php'),
-            ], 'migrations');
-        }
+        $this->createMigrations();
+        $this->setUpConfig();
     }
 
     /**
@@ -26,5 +21,35 @@ class JetpackServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    /**
+     * @return void
+     */
+    protected function createMigrations()
+    {
+        if (! class_exists('CreateJetpackTables')) {
+            $timestamp = date('Y_m_d_His', time());
+
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_jetpack_tables.php.stub' => database_path('migrations/'.$timestamp.'_create_jetpack_tables.php'),
+            ], 'migrations');
+        }
+
+        return;
+    }
+
+    /**
+     * @return void
+     */
+    protected function setUpConfig()
+    {
+        $source = dirname(__DIR__) . '/resources/config/jetpack.php';
+
+        $this->publishes([$source => config_path('jetpack.php')], 'config');
+
+        $this->mergeConfigFrom($source, 'sluggable');
+
+        return;
     }
 }
